7fef2f027f2e4e84f02029295da92b4a671c5494
Tadeo-AR26 <alonsoruiztadeo26@gmail.com>
2023-06-01 18:41:19 -0300

    Calculadora Completada

--- Program.cs
+++ Program.cs
@@ -1,7 +1,43 @@
 ﻿using Espacio.Calculadoras;
 
 Calculadora cal = new Calculadora();
-cal.Sumar(15);
-Console.WriteLine(cal.Resultado);
-cal.Restar(8);
-Console.WriteLine(cal.Resultado);
+double a = 0;
+string? operacion;
+Console.WriteLine("Ingrese la operacion que desea realizar");
+Console.WriteLine("Suma: +");
+Console.WriteLine("Resta: -");
+Console.WriteLine("Multiplicacion: *");
+Console.WriteLine("Division: /");
+Console.WriteLine("Limpiar: L");
+Console.WriteLine("Salir: e");
+
+do{
+    operacion = Console.ReadLine();
+    switch(operacion){
+        case "+":
+            bool control = double.TryParse(Console.ReadLine(), out a);
+            cal.Sumar(a);
+            Console.WriteLine(cal.Resultado);
+            break;
+        case "-":
+            control = double.TryParse(Console.ReadLine(), out a);
+            cal.Restar(a);
+            Console.WriteLine(cal.Resultado);
+            break;
+        case "*":
+            control = double.TryParse(Console.ReadLine(), out a);
+            cal.Multiplicar(a);
+            Console.WriteLine(cal.Resultado);
+            break;
+        case "/":
+            control = double.TryParse(Console.ReadLine(), out a);
+            cal.Dividir(a);
+            Console.WriteLine(cal.Resultado);
+            break;
+        case "L":
+            control = double.TryParse(Console.ReadLine(), out a);
+            cal.Limpiar(a);
+            Console.WriteLine(cal.Resultado);
+            break;
+    }
+}while(operacion != "e");
